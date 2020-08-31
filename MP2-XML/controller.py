e87bd6fada0e2f18ec13a4661573b54c11af2b8f
ct123098 <chen-t18@mails.tsinghua.edu.cn>
2020-09-01 00:19:19 +0800

    MP2: insert & delete

--- MP2-XML/controller.py
+++ MP2-XML/controller.py
@@ -24,9 +24,38 @@ class DBController():
     def close(self):
         self.cursor.close()
         self.conn.close()
+        print("[INFO] connection closed.")
 
-    def create_tables(self):
-        pass
+    def reset_tables(self):
+        self.cursor.execute("DROP TABLE IF EXISTS Student;")
+        self.cursor.execute("DROP TABLE IF EXISTS Course;")
+        self.cursor.execute("DROP TABLE IF EXISTS Registration;")
+        self.cursor.execute("""
+            CREATE TABLE Student (
+                ID INT PRIMARY KEY,
+                Name TEXT,
+                Age INT,
+                Dept TEXT
+            );
+        """)
+        self.cursor.execute("""
+            CREATE TABLE Course (
+                CourseID INT PRIMARY KEY,
+                CourseName TEXT,
+                Capacity INT,
+                RemainCapacity INT,
+                CreditHour INT,
+                Requirement TEXT
+            );
+        """)
+        self.cursor.execute("""
+            CREATE TABLE Registration (
+                StudentID INT,
+                CourseID INT,
+                Grade INT
+            );
+        """)
+        print("[INFO] tables are reset.")
 
     def insert_student(self, ID, name, age, dept):
         '''
@@ -36,7 +65,19 @@ class DBController():
             age (int)
             dept (string)
         '''
-        pass
+        self.cursor.execute("""
+            INSERT INTO Student(ID, Name, Age, Dept)
+            VALUES (?, ?, ?, ?)
+        """, ID, name, age, dept)
+        self.conn.commit()
+
+    def delete_student(self, ID):
+        self.cursor.execute("""
+            DELETE FROM Student
+            WHERE ID = ?
+        """, ID)
+        self.conn.commit()
+
 
     def insert_course(self, ID, name, capacity, credit_hour, requirement):
         '''
@@ -47,13 +88,19 @@ class DBController():
             credit_hour (int)
             requirement (string)
         '''
-        pass
+        self.cursor.execute("""
+            INSERT INTO Course(CourseID, CourseName, Capacity, RemainCapacity, CreditHour, Requirement)
+            VALUES (?, ?, ?, ?, ?, ?)
+        """, ID, name, capacity, capacity, credit_hour, requirement)
+        self.conn.commit()
 
-    def delete_student(self, ID):
-        pass
 
     def delete_course(self, ID):
-        pass
+        self.cursor.execute("""
+            DELETE FROM Course
+            WHERE ID = ?
+        """, ID)
+        self.conn.commit()
 
     def enrolled_student(self, courseID):
         '''
--- MP2-XML/run.py
+++ MP2-XML/run.py
@@ -3,6 +3,15 @@ import controller
 def main():
     ctrl = controller.DBController()
     ctrl.open(server="localhost,10913", database="mydatabase", uid="user", pwd="password")
+    ctrl.reset_tables()
+    ctrl.insert_student(1000113, "Wade Williams", 20, "CS")
+    ctrl.insert_student(1000214, "Dave Harris", 20, "CS")
+    ctrl.insert_student(1002098, "Ivan Scott", 18, "EE")
+    ctrl.insert_student(1002079, "Daisy White", 21, "EE")
+    ctrl.insert_student(1003342, "Lucy Clark", 21, "CS")
+    ctrl.insert_course(101, "Introduction to CS", 4, 3, "<Req></Req>")
+    ctrl.insert_course(102, "C++", 3, 3, "<Req><PrerequisiteCourse>101</PrerequisiteCourse><Dept>CS</Dept></Req>")
+    ctrl.close()
 
 if __name__ == "__main__":
     main()
